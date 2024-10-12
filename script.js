28fda6302345d625a3482d139fd5752c1dc22631
Andrii Morozov <morozov@ztu.edu.ua>
2024-10-12 09:44:41 +0300

    Create application for creating objects on webpage

---
+++ script.js
@@ -0,0 +1,123 @@
+let activeElement = null;
+let mouseCoord = {x: 0, y: 0};
+document.documentElement.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+    showContextMenu(event.clientX, event.clientY);
+    if (event.target.classList.contains('circle'))
+        return;
+    let circleElement = document.createElement('div');
+    circleElement.classList.add('circle');
+    document.body.appendChild(circleElement);
+    let width = parseInt(getComputedStyle(circleElement).width);
+    let height = parseInt(getComputedStyle(circleElement).height);
+    circleElement.style.top = (event.clientY - width / 2) + 'px';
+    circleElement.style.left = (event.clientX - height / 2) + 'px';
+    setActiveElement(circleElement);
+});
+
+function showContextMenu(x, y) {
+    let menuElement = document.querySelector('#contextmenu');
+    menuElement.style.display = 'block';
+    menuElement.style.left = x + 'px';
+    menuElement.style.top = y + 'px';
+}
+function hideContextMenu() {
+    let menuElement = document.querySelector('#contextmenu');
+    menuElement.style.display = 'none';
+}
+
+function moveElement(element, dx, dy) {
+    let leftCoord = parseInt(getComputedStyle(element).left);
+    let topCoord = parseInt(getComputedStyle(element).top);
+    leftCoord += dx;
+    topCoord += dy;
+    element.style.left = leftCoord + 'px';
+    element.style.top = topCoord + 'px';
+}
+
+function setActiveElement(element) {
+    if (activeElement === element)
+        return;
+    element.classList.add('active');
+    if (activeElement !== null)
+        activeElement.classList.remove('active');
+    activeElement = element;
+}
+
+document.documentElement.addEventListener('keydown', (event) => {
+    event.preventDefault();
+    if (activeElement === null)
+        return;
+    switch (event.code) {
+        case 'ArrowUp':
+            moveElement(activeElement, 0, -10);
+            break;
+        case 'ArrowDown':
+            moveElement(activeElement, 0, 10);
+            break;
+        case 'ArrowLeft':
+            moveElement(activeElement, -10, 0);
+            break;
+        case 'ArrowRight':
+            moveElement(activeElement, 10, 0);
+            break;
+        case 'Tab':
+            if (event.shiftKey) {
+                if (activeElement.previousElementSibling !== null &&
+                    activeElement.previousElementSibling.classList.contains('circle'))
+                    setActiveElement(activeElement.previousElementSibling);
+                else
+                    setActiveElement(document.querySelector('.circle:last-child'));
+            } else {
+                if (activeElement.nextElementSibling !== null &&
+                    activeElement.nextElementSibling.classList.contains('circle')) {
+                    setActiveElement(activeElement.nextElementSibling);
+                } else {
+                    setActiveElement(document.querySelector('.circle'));
+                }
+            }
+            break;
+    }
+});
+
+let isMove = false;
+
+document.documentElement.addEventListener('mousedown', (event) => {
+    if (!event.target.classList.contains('circle'))
+        return;
+    setActiveElement(event.target);
+    mouseCoord.x = event.clientX;
+    mouseCoord.y = event.clientY;
+    isMove = true;
+});
+
+document.documentElement.addEventListener('mousemove', (event) => {
+    if (!isMove)
+        return;
+    let coordX = event.clientX;
+    let coordY = event.clientY;
+    let dx = coordX - mouseCoord.x;
+    let dy = coordY - mouseCoord.y;
+    mouseCoord.x = coordX;
+    mouseCoord.y = coordY;
+    moveElement(activeElement, dx, dy);
+});
+
+document.documentElement.addEventListener('mouseup', (event) => {
+   isMove = false;
+});
+
+document.documentElement.addEventListener('click', (event) => {
+    let currentTag = event.target;
+    event.preventDefault();
+    if (currentTag.tagName === 'A' && currentTag.closest('#contextmenu'))
+    {
+        activeElement.classList.remove(...activeElement.classList);
+        activeElement.classList.add('circle');
+        activeElement.classList.add('active');
+        activeElement.classList.add(
+            currentTag.getAttribute('href').slice(1)
+        )
+    } else
+        hideContextMenu();
+});
